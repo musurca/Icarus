132723593f46658aeaae547a0846958b8140617d
Nicholas Musurca <nick.musurca@gmail.com>
2020-04-13 01:36:36 -0700

    airport info

---
+++ airportinfo.py
@@ -0,0 +1,96 @@
+'''
+airportinfo.py
+
+Displays relevant information about an airport: radio frequencies, runways, etc.
+
+'''
+
+import csv
+import sys
+
+from math import sin, cos, sqrt, atan2, radians
+
+DATA_DIR = "./data/"
+
+# distance between two global points in nautical miles
+def dist_coord(lat1,lon1,lat2,lon2): 
+    # source: https://stackoverflow.com/questions/19412462/getting-distance-between-two-points-based-on-latitude-longitude  
+    R = 6373.0 # approximate radius of earth in km
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    return 0.539957*R * c
+
+if len(sys.argv) > 1:
+    code = sys.argv[1].upper()
+else:
+    sys.exit("You must provide an ICAO airport code!")
+
+# get basic info
+with open(DATA_DIR+'airports.csv', newline='') as csvfile:
+    airports = csv.DictReader(csvfile)
+    for airport in airports:
+        ident = airport['ident']
+        if ident == code:
+            apLat = float(airport['latitude_deg'])
+            apLong = float(airport['longitude_deg'])
+            apName = airport['name']
+            apId = airport['id']
+            apElev = airport['elevation_ft']
+
+print("")
+print(apName + " (" + code + ")")
+if apLat >= 0:
+    latText = str(round(abs(apLat),6)) + "°N"
+else:
+    latText = str(round(abs(apLat),6)) + "°S"
+if apLong >= 0:
+    longText = str(round(abs(apLong),6)) + "°E"
+else:
+    longText = str(round(abs(apLong),6)) + "°W"
+print(latText + ", " + longText + ", elev. " + apElev + " ft ASL")
+print("------------------------------------------------------")
+
+# runways
+with open(DATA_DIR+'runways.csv', newline='') as csvfile:
+    runways = csv.DictReader(csvfile)
+    for runway in runways:
+        refId = runway['airport_ident']
+        if refId == code and runway['closed'] == "0":
+            print("Runway " + runway['le_ident'] + " (" + runway['le_heading_degT'] + "°) / " + runway['he_ident'] + " (" + runway['he_heading_degT'] + "°) -- " + runway['length_ft'] + " ft")
+
+# navaids
+print("")
+with open(DATA_DIR+'navaids.csv', newline='') as csvfile:
+    navaids = csv.DictReader(csvfile)
+    for navaid in navaids:
+        refId = navaid['associated_airport']
+        if refId == code:
+            navLat = float(navaid['latitude_deg'])
+            navLong = float(navaid['longitude_deg'])
+            dist = dist_coord(apLat, apLong, navLat, navLong)
+            if navaid['frequency_khz'] != "":
+                if navaid['type'] == "NDB":
+                    freq = navaid['frequency_khz'] + " mHz"
+                else:
+                    freq = str(float(navaid['frequency_khz'])/1000) + " mHz"
+            else:
+                freq = ""
+            print(navaid['ident'] + " (" + navaid['name'] + " " + navaid['type'] + ") @ " + freq + " -- " + str(round(dist,2)) + " nm")
+
+# com frequencies
+print("")
+with open(DATA_DIR+'airport-frequencies.csv', newline='') as csvfile:
+    freqs = csv.DictReader(csvfile)
+    for freq in freqs:
+        refId = freq['airport_ident']
+        if refId == code:
+            print(freq['type'] + "\t" + freq['frequency_mhz'] + " mHz\t(" + freq['description'] + ")")
+
+
+print("")
--- distance.py
+++ distance.py
@@ -17,27 +17,57 @@ DATA_DIR = "./data/"
 if len(sys.argv) > 2:
     src = sys.argv[1].upper()
     dst = sys.argv[2].upper()
-
-    if len(src) != 4 or len(dst) != 4:
-        sys.exit("You must provide four-letter ICAO codes!")
+    if len(sys.argv) > 3:
+        region = sys.argv[3].upper()
+        anyRegion = False
+    else:
+        region = ""
+        anyRegion = True
 else:
-    sys.exit("You must provide two four-letter airport ICAO codes!")
+    sys.exit("You must provide two ICAO airport/navaid codes and an (optional) region code!")
 
-# load airports
+srcLat = None
+dstLat = None
+
+# try airports first
 with open(DATA_DIR+'airports.csv', newline='') as csvfile:
     airports = csv.DictReader(csvfile)
     for airport in airports:
         ident = airport['ident']
-        if ident == src:
+        country = airport['iso_country']
+        if ident == src and (anyRegion or region==country):
             srcLat = float(airport['latitude_deg'])
             srcLong = float(airport['longitude_deg'])
             srcName = airport['name']
-        elif ident == dst:
+        elif ident == dst and (anyRegion or region==country):
             dstLat = float(airport['latitude_deg'])
             dstLong = float(airport['longitude_deg'])
             dstName = airport['name']
 
-# distance between two global points in kilometers
+# if we don't have enough data, try navaids next
+if srcLat == None or dstLat == None:
+    with open(DATA_DIR+'navaids.csv', newline='') as csvfile:
+        navaids = csv.DictReader(csvfile)
+        for navaid in navaids:
+            ident = navaid['ident']
+            country = navaid['iso_country']
+            if ident == src and (anyRegion or region==country):
+                srcLat = float(navaid['latitude_deg'])
+                srcLong = float(navaid['longitude_deg'])
+                srcName = navaid['name']
+            elif ident == dst and (anyRegion or region==country):
+                dstLat = float(navaid['latitude_deg'])
+                dstLong = float(navaid['longitude_deg'])
+                dstName = navaid['name']
+
+if srcLat == None or dstLat == None:
+    if srcLat == None:
+        print("Can't find " + src + "!")
+    if dstLat == None:
+        print("Can't find " + dst + "!")
+    sys.exit()
+
+# distance between two global points in nautical miles
 def dist_coord(lat1,lon1,lat2,lon2): 
     # source: https://stackoverflow.com/questions/19412462/getting-distance-between-two-points-based-on-latitude-longitude  
     R = 6373.0 # approximate radius of earth in km
@@ -49,13 +79,9 @@ def dist_coord(lat1,lon1,lat2,lon2):
     dlat = lat2 - lat1
     a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    return R * c
-
-# convert kilometers to nautical miles
-def km_to_nm(n):
-    return 0.539957*n
+    return 0.539957*R * c
 
 dist = dist_coord(srcLat,srcLong,dstLat,dstLong)
 
 print(src + " (" + srcName + ") --> " + dst + " (" + dstName + ")")
-print(str(round(km_to_nm(dist),2)) + " nm")
+print(str(round(dist,2)) + " nm")
