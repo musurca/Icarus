eba17c7d5e617711357e3019ce735e7ca1090103
Nicholas Musurca <nick.musurca@gmail.com>
2020-05-05 02:58:42 -0700

    Delete guitest.py

--- guitest.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-master = Tk()
-master.title("FAA Airport Diagram")
-Label(master, text='Airport ICAO Code').grid(row=0) 
-getBtn = Button(master, text='Download').grid(row=1)
-e1 = Entry(master) 
-e1.grid(row=0, column=1) 
-mainloop()
